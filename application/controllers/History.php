2279d997fa7255d636570a2d308ce35295ad670e
Domyouji <tommytrann@hotmail.com>
2017-04-02 12:00:30 -0700

    Created two buttons for filtering

--- application/controllers/History.php
+++ application/controllers/History.php
@@ -16,26 +16,74 @@ class History extends Application
 	{
 		$role = $this->session->userdata('userrole');
 		
-    if($role == 'boss' || $role == 'supervisor') {
-		// this is the view we want shown
-		$source = $this->History_model->getData();
-		//$this->view('history',$source);
-		//var_dump($source);	
+		if($role == 'boss' || $role == 'supervisor') {
+			// this is the view we want shown
+			$source = $this->History_model->getData();
+			//$this->view('history',$source);
+			//var_dump($source);	
+			$history = array ();
+			foreach ($source as $record)
+			{
+				$history[] = array ('id' => $record['id'],
+									'model' => $record['model'] . $record['piece'],
+									'plant' => $record['plant'],
+									'stamp' => $record['stamp'],
+									);
+			}
+			$this->data['history'] = $history;
+			$this->data['pagebody'] = 'history';
+
+			$this->render();
+		} else {
+				redirect('welcome');
+		  }
+	}
+	
+	public function dateSort(){
+		$role = $this->session->userdata('userrole');
+		
+		if($role == 'boss' || $role == 'supervisor') {
+		$sortDate = $this->History_model->sortingDate();
+		
 		$history = array ();
-		foreach ($source as $record)
-		{
-			$history[] = array ('id' => $record['id'],
-								'model' => $record['model'] . $record['piece'],
-								'plant' => $record['plant'],
-								'stamp' => $record['stamp'],
-								);
-		}
+			foreach ($sortDate as $record)
+			{
+				$history[] = array ('id' => $record['id'],
+									'model' => $record['model'] . $record['piece'],
+									'plant' => $record['plant'],
+									'stamp' => $record['stamp'],
+									);
+			}
+			
 		$this->data['history'] = $history;
 		$this->data['pagebody'] = 'history';
-
 		$this->render();
-    } else {
-			redirect('welcome');
-	  }
+		} else {
+				redirect('welcome');
+		}
+	}
+		
+	public function modelSort(){
+		$role = $this->session->userdata('userrole');
+		
+		if($role == 'boss' || $role == 'supervisor') {
+		$sortModel = $this->History_model->sortingModel();
+		
+		$history = array ();
+			foreach ($sortModel as $record)
+			{
+				$history[] = array ('id' => $record['id'],
+									'model' => $record['model'] . $record['piece'],
+									'plant' => $record['plant'],
+									'stamp' => $record['stamp'],
+									);
+			}
+			
+		$this->data['history'] = $history;
+		$this->data['pagebody'] = 'history';
+		$this->render();
+		} else {
+				redirect('welcome');
+		  }
 	}
 }
--- application/views/history.php
+++ application/views/history.php
@@ -1,13 +1,20 @@
+<div>
+<form>
+	<button type="submit" id="filter" class="btn btn-primary btn-sm" formaction="/History/dateSort">Sort by date</button>
+	<button type="submit" id="filter" class="btn btn-primary btn-sm" formaction="/History/modelSort">Sort by robot model</button>
+</form>
+</div>
+
 <div class="container">
   <h2>History</h2>                                            
   <div class="table-responsive">          
-  <table class="table">
+  <table class="table table-striped table-bordered">
     <thead>
       <tr>
         <th>Id</th>
         <th>Model</th>
 		<th>Plant</th>
-		<th>Stamp</th>
+		<th>Date</th>
       </tr>
     </thead>
     <tbody>
